be9c77c8b1df714a1e7377e0f5dbf3f3ac3f9437
Louis Emery <emerylo@educ.college-montreal.qc.ca>
2023-10-24 16:18:49 -0400

    Enemy movement

--- Ikorni/Assets/Scripts/Enemies/Enemy.cs
+++ Ikorni/Assets/Scripts/Enemies/Enemy.cs
@@ -4,15 +4,27 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public int hp;
+    public int moveSpeed;
+    public Transform player;
+    private Rigidbody2D rb;
+    private Vector2 movement;
     void Start()
     {
-        
+        rb = this.GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    void Update(){
+        Vector3 direction = player.position - transform.position;
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        rb.rotation = angle;
+        direction.Normalize();
+        movement = direction;
+    }
+    private void FixedUpdate() {
+        moveCharacter(movement);
+    }
+    void moveCharacter(Vector2 direction){
+        rb.MovePosition((Vector2)transform.position + (direction * moveSpeed * Time.deltaTime));
     }
 }
--- Ikorni/Assets/Scripts/Enemies/Melee.cs
+++ Ikorni/Assets/Scripts/Enemies/Melee.cs
@@ -2,12 +2,22 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Melee : MonoBehaviour
+public class Melee : Enemy
 {
+    public int attackSpeed;
+    public int attackRange;
+    [HideInInspector]
+    public float timeBetweenAttack;
+
+    public CapsuleCollider2D attackRangeCollider;
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        float resizedRange = 0.1111111f * attackRange + 0.8888888f;
+        Debug.Log(resizedRange);
+        timeBetweenAttack = 10/attackSpeed;
+        attackRangeCollider.size = new Vector2(resizedRange, resizedRange+1);
     }
 
     // Update is called once per frame
