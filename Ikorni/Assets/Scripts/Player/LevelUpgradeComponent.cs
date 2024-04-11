219eb175e906a9c487192acfab1e525b100e4b78
ManechLepage <113209819+ManechLepage@users.noreply.github.com>
2024-04-11 10:33:57 -0400

    Update LevelUpgradeComponent.cs

--- Ikorni/Assets/Scripts/Player/LevelUpgradeComponent.cs
+++ Ikorni/Assets/Scripts/Player/LevelUpgradeComponent.cs
@@ -2,5 +2,19 @@ using UnityEngine;
 
 public class LevelUpgradeComponent : MonoBehaviour
 {
-    
+    public GameObject upgradePanel;
+    public PlayerData playerData;
+    public bool isUpgrading = false;
+
+    public void Upgrade(int lvl)
+    {
+        isUpgrading = true;
+        upgradePanel.SetActive(true);
+    }
+
+    public void DeActivateUpgradePanel()
+    {
+        isUpgrading = false;
+        upgradePanel.SetActive(false);
+    }
 }
