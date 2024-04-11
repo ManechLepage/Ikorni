cc553f82055c1113aed2043099f044266aed6d7d
Louis Emery <emerylo@educ.college-montreal.qc.ca>
2024-04-11 12:09:01 -0400

    scriptable waves

---
+++ Ikorni/Assets/Scripts/Enemies/EnemyWave.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[CreateAssetMenu(fileName = "EnemyWave")]
+
+public class EnemyWave : ScriptableObject
+{
+    public int meleeEnemyCount;
+    public int rangedEnemyCount;
+    public int scalePercentage;
+    
+}
